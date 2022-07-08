19a894b60af1bc3e0da3c653c5e01b8d2944d9b6
Lucas Welscher <ga53foy@mytum.de>
2022-07-08 22:36:15 +0200

    Add logging to template

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -1,5 +1,7 @@
 package de.tum.in.ase.eist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api", produces = {MediaType.TEXT_PLAIN_VALUE})
 public class AnswerResource {
 
+	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
+
 	private final QueryProcessor queryProcessor;
 
 	public AnswerResource(QueryProcessor queryProcessor) {
@@ -19,6 +23,7 @@ public class AnswerResource {
 
 	@GetMapping("*")
 	public ResponseEntity<String> answer(@RequestParam("q") String query) {
+		logger.info("Received query \"{}\"", query);
 		String answer = queryProcessor.process(query);
 		return ResponseEntity.ok(answer);
 	}
